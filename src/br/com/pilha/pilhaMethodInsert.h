2cb4c77b9652ea1c67871995446c5647464e6d12
uires <uiresdeivide1@gmail.com>
2018-03-15 22:47:06 -0300

    Novas funções

--- src/br/com/pilha/pilhaMethodInsert.h
+++ src/br/com/pilha/pilhaMethodInsert.h
@@ -8,7 +8,7 @@ typedef struct{
 }TP_PILHA;
 
 void inicializaPilha(TP_PILHA * pointerPilha){
-	printf("Inicializado Pilha!\n");
+	//printf("Inicializado Pilha!\n");
 	pointerPilha->topo = -1;
 }
 int isEmpty(TP_PILHA * pointerPilha){
@@ -30,10 +30,10 @@ int alturaPilha(TP_PILHA pointerPilha){
 }
 int push(TP_PILHA * pointerPilha, int value){
 	if(isFull(pointerPilha) == 1){
-		printf("Is full!\n");
+		//printf("Is full!\n");
 		return 0;
 	}else{
-		printf("Inserido o valor...: %d, no topo ...: %d \n", value, pointerPilha->topo + 2 );
+		//printf("Inserido o valor...: %d, no topo ...: %d \n", value, pointerPilha->topo + 2 );
 		pointerPilha->topo++;
 		pointerPilha->item[pointerPilha->topo] = value;
 	}
@@ -52,12 +52,12 @@ void listaPilha(TP_PILHA pointerPilha){
 */
 int pop(TP_PILHA * pointerPilha, tp_item * e){
 	if(isEmpty(pointerPilha) == 1){
-		printf("Is empty\n!");
+		//printf("Is empty\n!");
 		return 0;
 	}else{
 		*e = pointerPilha->item[pointerPilha->topo];
 		pointerPilha->topo--;
-		printf("Remove item...: %d\n", pointerPilha->topo);
+		//printf("Remove item...: %d\n", pointerPilha->topo);
 		
 		return 1;
 	}
@@ -84,27 +84,67 @@ int desempilhaTotal(TP_PILHA * pointerPilha){
 	usando o comportamento de pop e push;
 */
 
-void retiraImpares(TP_PILHA pointerPilha){
+void retiraImpares(TP_PILHA * pointerPilha){
 	TP_PILHA pilhaAuxiliar;
 	inicializaPilha(&pilhaAuxiliar);
 	tp_item e;
 	
-	while(isEmpty(&pointerPilha) > -1){
-		pop(&pointerPilha, &e);
+	while(!isEmpty(pointerPilha)){
+		pop(pointerPilha, &e);
 		if(e % 2 == 0){
 			push(&pilhaAuxiliar, e);
 		}else{
 			continue;
 		}
 	}
-	
-	while(isEmpty(&pilhaAuxiliar) > -1){
+	while(!isEmpty(&pilhaAuxiliar)){
 		pop(&pilhaAuxiliar, &e);
-		push(&pointerPilha, e);
+		push(pointerPilha, e);
 	}
 	
 }
 
+/*
+	@author u'ires
+	date 2018-03-15
+	
+	Essa função tem a função de imprimir uma pilha utilizando o comportamento correto de desimpilhar
+	, seguindo a seguinte assinatura int imprimir(TP_PILHA PILHA);
+*/
+int imprimirPilha(TP_PILHA pilha){
+	if(isEmpty(&pilha) == 1){
+		return 0;
+	}else{
+		tp_item e;
+		while(!isEmpty(&pilha)){
+			pop(&pilha, &e);
+			printf("[ %d ]...-> %d\n", alturaPilha(pilha), e);
+		}
+		return 1;
+	}
+	
+}
+/* 
+	@uthor uíres
+	date 2018-03-15
+	
+	Essa função compara duas pilhas e retorna 1 caso sejam iguais e 0 se não forem iguais
+*/
 
-
-
+int comparaPilha(TP_PILHA pointerPilhaUm, TP_PILHA pointerPilhaDois){
+	tp_item x, y;
+	if(alturaPilha(pointerPilhaUm) != alturaPilha(pointerPilhaDois)){
+		return 0;
+	}else{
+		while(!isEmpty(&pointerPilhaUm) && !isEmpty(&pointerPilhaDois)){
+			pop(&pointerPilhaUm, &x);
+			pop(&pointerPilhaDois, &y);
+			if(x != y){
+				return 0;
+			}else{
+				continue;
+			}
+		}
+		return 1;		
+	}
+}
